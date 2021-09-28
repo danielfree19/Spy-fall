00d5170f8986370c3ab254e26e64982c005d7f23
arkadi y <arkadiyordan@gmail.com>
2021-09-28 12:51:11 +0300

    removed dup class

--- app/src/main/java/com/danielinc/spyfall/Config.java
+++ app/src/main/java/com/danielinc/spyfall/Config.java
@@ -26,11 +26,9 @@ import android.widget.Toast;
  */
 public class Config extends Fragment {
 
-
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
 
     private String mParam1;
     private String mParam2;
